72ce94618dfafbf8b4a0091b070ae259338ec047
AYnonyme <110267060+AYnonyme971@users.noreply.github.com>
2023-06-21 20:16:05 -0400

    Add files via upload

--- en/Array.js
+++ en/Array.js
@@ -1,11 +1,19 @@
 const array = [
     {
-        joke: "Why the girafes have a long neck?",
-        response: "Because they have smelly feets!"
+        joke: "Why the girafes have a long neck",
+        response: "Because they have smelly feets"
     },
     {
-        joke: "Which animal have the more teeths?",
-        response: "The little mouse!"
+        joke: "Which animal have the more teeths",
+        response: "The little mouse"
+    },
+    {
+        joke: "What's the best thing about Switzerland",
+        response: "I don't know, but the flag is a big plus"
+    },
+    {
+        joke: "Hear about the new restaurant called Karma",
+        response: "There's no menu: you get what you deserve"
     }
 ];
 
--- fr/Array.js
+++ fr/Array.js
@@ -4,12 +4,72 @@ const array = [
         reponse: false
     },
     {
-        blague: "Pourquoi les girafes ont-elles un long cou ?",
-        reponse: "Car elles puent des pieds !"
+        blague: "Pourquoi les girafes ont-elles un long cou",
+        reponse: "Car elles puent des pieds"
     },
     {
-        blague: "Quel animal a le plus de dents ?",
-        reponse: "La petite souris !"
+        blague: "Quel animal a le plus de dents",
+        reponse: "La petite souris"
+    },
+    {
+        blague: "Tu connais la blague du ptit dej",
+        reponse: "Pas de bol"
+    },
+    {
+        blague: "Comment appelle-t-on une chauve-souris avec une perruque",
+        reponse: "Une souris"
+    },
+    {
+        blague: "Avec quoi on récupère la papaye",
+        reponse: "Avec une foufourche"
+    },
+    {
+        blague: "Qu'est-ce qui n'est pas un steak",
+        reponse: "Une pastèque"
+    },
+    {
+        blague: "Quelle est la mamie qui fait le plus peur",
+        reponse: "Mamie Traillete"
+    },
+    {
+        blague: "Comment appelle-t-on un bébé éléphant prématuré",
+        reponse: "Un éléphant tôt"
+    },
+    {
+        blague: "Tu connais l'histoire du lit superposé",
+        reponse: "Elle est à dormir debout"
+    },
+    {
+        blague: "Comment appelle-t-on un ascenseur",
+        reponse: "Avec le bouton"
+    },
+    {
+        blague: "Comment on appelle un nain facteur",
+        reponse: "Un nain posteur"
+    },
+    {
+        blague: "De quoi a besoin un astronaute claustrophobe",
+        reponse: "D'un peu d'espace"
+    },
+    {
+        blague: "Comment appelle-t-on un matelôt bourré",
+        reponse: "Un soûl marin"
+    },
+    {
+        blague: "Avec quoi les poissons payent leus courses",
+        reponse: "Avec un sou-marin"
+    },
+    {
+        blague: "Quel est le sport préféré des électriciens",
+        reponse: "Le karaté : ils connaissent toutes les prises"
+    },
+    {
+        blague: "Comment appelle-t-on un yaourt à la campagne",
+        reponse: "Un yaourt nature"
+    },
+    {
+        blague: "Quelle est la meilleure chose en Suisse",
+        reponse: "Je ne sais pas, mais en tout cas le drapeau est un plus"
     }
 ];
 
